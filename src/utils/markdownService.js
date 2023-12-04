11383df842b9ea508392011a1e43f371f15bc56e
Rinosh <rinosh@deskfactors.com>
2023-12-04 10:24:12 +0530

    removed console logs

--- src/utils/markdownService.js
+++ src/utils/markdownService.js
@@ -14,12 +14,7 @@ const convertMarkdownToHTML = async (markdown) => {
         }
 
         const { html } = await response.json();
-
-
-        // Use the 'html' received from the backend to display/render the converted content
-        console.log(html); // Display the converted HTML
         return html
-        // Update your UI to show the converted HTML (e.g., set it in a state for rendering)
     } catch (error) {
         console.error('Error converting Markdown to HTML:', error.message);
     }
